db940bd7795d7a619531355a0d781678ad682ddf
KICGunathilaka <isurugunathilaka1@gmail.com>
2025-11-08 17:56:26 +0530

    create login page with sucess login

---
+++ frontend/src/components/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return (
+    <div className="dashboard-container">
+      <h1>Dashboard</h1>
+      <p>Welcome! You can build out this page later.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
